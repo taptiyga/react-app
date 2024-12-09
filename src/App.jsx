41714b5fdf6803258fe4dcb549434525f1c57029
Taptiyga <48513295+taptiyga@users.noreply.github.com>
2024-12-09 14:59:37 +0400

    5.2 Add Item

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,32 @@ import LeftPanel from './layouts/LeftPanel/LeftPanel'
 import JournalList from './components/JournalList/JournalList'
 import JournalAddButton from './components/JournalAddButton/JournalAddButton'
 import JournalForm from './components/JournalForm/JournalForm'
+import { useState } from 'react'
 
+const INITIAL_DATA = [
+  {
+    title: 'Запись №1',
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae suscipit, laudantium commodi est id beatae quo error, esse, libero voluptate accusantium voluptas nulla non velit nobis mollitia fuga sint magnam.',
+    date: new Date()
+  },
+  {
+    title: 'Запись №2',
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae suscipit',
+    date: new Date()
+  },
+]
 
 function App() {
-  const data = [
-    {
-      title: 'Запись №1',
-      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae suscipit, laudantium commodi est id beatae quo error, esse, libero voluptate accusantium voluptas nulla non velit nobis mollitia fuga sint magnam.',
-      date: new Date()
-    },
-    {
-      title: 'Запись №2',
-      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae suscipit',
-      date: new Date()
-    },
-  ]
+  const [items, setItems] = useState(INITIAL_DATA)
+
+  const addItem = item => {
+    setItems(oldItems => [...oldItems, {
+      text: item.text,
+      title: item.title,
+      date: new Date(item.date)
+    }])
+  }
+
 
   return (
     <div className='app'>
@@ -29,24 +40,19 @@ function App() {
         <Header />
         <JournalAddButton />
         <JournalList>
-          <CardButton>
-            <JournalItem
-              title={data[0].title}
-              text={data[0].text}
-              date={data[0].date}
-            />
-          </CardButton>
-          <CardButton>
-            <JournalItem
-              title={data[1].title}
-              text={data[1].text}
-              date={data[1].date}
-            />
-          </CardButton>
+          {items.map(el => (
+            <CardButton>
+              <JournalItem
+                title={el.title}
+                text={el.text}
+                date={el.date}
+              />
+            </CardButton>
+          ))}
         </JournalList>
       </LeftPanel>
       <Body>
-        <JournalForm />
+        <JournalForm onSubmit={addItem} />
       </Body>
     </div>
   )
