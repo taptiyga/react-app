7a6286b648b21bf4e1183aee3ba9a08b9f2ab64e
Taptiyga <48513295+taptiyga@users.noreply.github.com>
2024-12-09 11:16:34 +0400

    4.4 decomposition

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,17 @@
 import './Button.css'
+import { useState } from 'react'
 
 function Button() {
+    const [text, setText] = useState('Открыть')
+    console.log('Rerender')
+
+    const clicked = () => {
+        setText(t => t + '!')
+        console.log(text)
+    }
 
     return (
-        <button className='button accent'>Сохранить</button>
+        <button onClick={clicked} className='button accent'>{text}</button>
     )
 }
 
---
+++ src/components/JournalAddButton/JournalAddButton.jsx
@@ -0,0 +1,13 @@
+import './JournalAddButton.css'
+import CardButton from '../CardButton/CardButton'
+
+function JournalAddButton() {
+
+    return (
+        <CardButton>
+            Новое сообщение
+        </CardButton>
+    )
+}
+
+export default JournalAddButton
